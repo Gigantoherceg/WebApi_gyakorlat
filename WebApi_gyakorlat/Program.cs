151c4ca56045877ef120de908ed2d992e5e0a7c0
Gábor Mészáros <mgaboor@gmail.com>
2023-03-10 12:51:21 +0100

    Add Authorization to program.cs and the database migration

--- WebApi_gyakorlat/Program.cs
+++ WebApi_gyakorlat/Program.cs
@@ -6,13 +6,24 @@ namespace WebApi_gyakorlat
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("No connectionString");
             builder.Services.AddDbContext < GyakDbContext > (options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddIdentityCore<ApplicationUser>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+                            .AddEntityFrameworkStores < GyakDbContext > ();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -34,6 +45,12 @@ namespace WebApi_gyakorlat
 
 
             app.MapControllers();
+
+            using (var scope = app.Services.CreateScope())
+            using (var context = scope.ServiceProvider.GetRequiredService < GyakDbContext > ())
+            {
+                await context.Database.MigrateAsync();
+            }
 
             app.Run();
         }
